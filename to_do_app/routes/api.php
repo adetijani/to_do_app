45619138ba53c9115c59642c974bd5e7e1b8d5fe
Adebowale Tijani <tijanidebo@gmail.com>
2024-08-19 15:29:45 +0100

    Updated middleware

--- to_do_app/routes/api.php
+++ to_do_app/routes/api.php
@@ -1,8 +1,16 @@
 <?php
-
 use Illuminate\Http\Request;
+use App\Http\Middleware\routeconfig;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TodoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::group('/', function () {
+    Route::get('all/todo', [TodoController::class,'index']);
+    Route::post('store/todo', [TodoController::class,'store']);
+    Route::patch('edit/todo', [TodoController::class,'update']);
+    Route::get('show/{todo}', [TodoController::class,'show']);
+    Route::delete('delete/todo', [TodoController::class,'destroy']);
+})->middleware(routeconfig::class);
+
